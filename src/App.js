38ee2c3100718ae7a97fa05b415445ce9fe69c06
Tanish Narang <narangtanish2@gmail.com>
2024-08-09 21:15:34 +0530

    The classnames library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { click } from "@testing-library/user-event/dist/click";
+// import { click } from "@testing-library/user-event/dist/click";
 import Button from "./components/Button";
 export default function App() {
   return (
     <div>
-      <Button>Click me!</Button>
-      <Button>Press ME!</Button>
-      <Button>hover Me!</Button>
-      <Button>primary</Button>
-      <Button>Secondary</Button>
+      <Button primary>Click me!</Button>
+      <Button secondary>Press ME!</Button>
+      <Button success>hover Me!</Button>
+      <Button warning>primary</Button>
+      <Button danger>Secondary</Button>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,24 @@
-export default function Button({ children }) {
+import classNames from "classnames";
+export default function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  rounded,
+  outline,
+}) {
+  const classes = classNames("px-3 py-1.5 border", {
+    "border-blue-600 bg-blue-500 text-white": primary,
+    "border-gray-900 bg-gray-900 text-white": secondary,
+    "border-green-500 bg-green-500 text-white": success,
+    "border-yellow-500 bg-yellow-500 text-white": warning,
+    "border-red-500 bg-red-500 text-white": danger,
+  });
   return (
     <div>
-      <button>{children}</button>
+      <button className={classes}>{children}</button>
     </div>
   );
 }
