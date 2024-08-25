b2cd897c030f84db9dcccfbdab18cda084129f8c
Tanish Narang <narangtanish2@gmail.com>
2024-08-25 17:00:15 +0530

    Community Convention with Props Names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 export default function App() {
+  const [selection, setSelection] = useState();
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
   const options = [
     { label: "Red", value: "red" },
     { label: "Green", value: "green" },
     { label: "Blue", value: "blue" },
   ];
 
-  return <Dropdown options={options} />;
+  return (
+    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,3 +1,30 @@
-export default function Dropdown({ options }) {
-  return <div>Dropdown..</div>;
+import { useState } from "react";
+// import { GoArrowLeft } from "react-icons/go";
+export default function Dropdown({ options, selection, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onSelect(option);
+  };
+
+  const renderedOption = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  const handleClick = () => {
+    setIsOpen((currentOpen) => !currentOpen);
+  };
+
+  return (
+    <div>
+      Select the option...
+      <div onClick={handleClick}>{selection?.label || "select..."}</div>
+      {isOpen && <div>{renderedOption}</div>}
+    </div>
+  );
 }
